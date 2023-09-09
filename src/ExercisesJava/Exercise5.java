29c84af651632153439d73dc4b7de04ab1c5d1a7
Gabriel Mendonça <gabrielmendonca3827@gmail.com>
2023-09-08 22:29:41 -0300

    Revert "Antecessor e sucessor de um número inteiro."
    This reverts commit c0d6375fe34ebd26d52af11b964a9f0a5625995e.

--- src/ExercisesJava/Exercise5.java
+++ src/ExercisesJava/Exercise5.java
@@ -1,13 +1,8 @@
-package com.br.gabriel.exercisesJava;
+package ExercisesJava;
 
 import java.text.NumberFormat;
 import java.util.Scanner;
-/*
- *  Crie um algoritmo que leia o valor do salário mínimo
- *  e o valor do salário de um usuário, calcule a quantidade
- *  de salários mínimos que esse usuário ganha e imprima o resultado.
- *  (1SM=R$788,00)
- */
+
 public class Exercise5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
