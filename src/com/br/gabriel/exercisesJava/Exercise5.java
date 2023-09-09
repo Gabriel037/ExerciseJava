c0d6375fe34ebd26d52af11b964a9f0a5625995e
Gabriel Mendonça <gabrielmendonca3827@gmail.com>
2023-09-08 22:24:48 -0300

    Antecessor e sucessor de um número inteiro.

--- src/com/br/gabriel/exercisesJava/Exercise5.java
+++ src/com/br/gabriel/exercisesJava/Exercise5.java
@@ -1,8 +1,13 @@
-package ExercisesJava;
+package com.br.gabriel.exercisesJava;
 
 import java.text.NumberFormat;
 import java.util.Scanner;
-
+/*
+ *  Crie um algoritmo que leia o valor do salário mínimo
+ *  e o valor do salário de um usuário, calcule a quantidade
+ *  de salários mínimos que esse usuário ganha e imprima o resultado.
+ *  (1SM=R$788,00)
+ */
 public class Exercise5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
