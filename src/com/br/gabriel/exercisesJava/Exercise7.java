d286911bc07f213702a448b8fdceb82ed4cdb964
Gabriel Mendonça <gabrielmendonca3827@gmail.com>
2023-09-08 23:19:46 -0300

    Eu gosto de estudar algoritmos ^_^ .

---
+++ src/com/br/gabriel/exercisesJava/Exercise7.java
@@ -0,0 +1,12 @@
+package com.br.gabriel.exercisesJava;
+/*
+ * Escreva um algoritmo que exiba 20 vezes a mensagem
+ * “Eu gosto de estudar Algoritmos!”.
+ */
+public class Exercise7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("[" + i + "]" + " Eu gosto de estudar Algoritmos!");
+        }
+    }
+}
